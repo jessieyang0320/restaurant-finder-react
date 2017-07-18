533a435cc199d1b61bc79712a42c7bfa68b5f99d
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-07-18 00:11:24 -0400

    add home header component to home page

---
+++ src/containers/Home/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+import HomeHeader from '../../components/HomeHeader/index.js'
+
+class Home extends React.Component {
+	constructor(props,context){
+		super(props,context);
+		this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+	}
+	render(){
+		return(
+			<div>
+				<HomeHeader/>
+
+			</div>)
+	}
+}
+
+export default Home
